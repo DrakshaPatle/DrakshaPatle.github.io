c7d918c9328e4d1cbcf0a8a7829ec9dc0fc9b6db
DrakshaPatle <draksha@appsdeployer.com>
2023-01-24 13:53:29 +0530

    secondui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Header from './components/Header/Header';
-import  {Yourneeds} from './components/pages/YourNeeds/Yourneeds'
+// import  {Yourneeds} from './components/pages/YourNeeds/Yourneeds'
+import  {Spacedetails} from './components/pages/SpaceDetails/Spacedetails'
 function App() {
   return (
     <>
      <Header/>
     
 
-     <Routes>
+     {/* <Routes>
         <Route path="/" element={<Yourneeds />} />
+      </Routes> */}
+
+      <Routes>
+        <Route path="/" element={<Spacedetails />} />
       </Routes>
+
     </>
   );
 }
